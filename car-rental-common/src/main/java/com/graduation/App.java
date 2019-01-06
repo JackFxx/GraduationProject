552819ec16730d633b63e042f9723651e48dd1c7
JackFxx <1320865120@qq.com>
2019-01-06 15:37:10 +0800

    毕业设计项目

---
+++ car-rental-common/src/main/java/com/graduation/App.java
@@ -0,0 +1,13 @@
+package com.graduation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ car-rental-web/src/main/java/com/graduation/carrentalweb/CarRentalWebApplication.java
@@ -0,0 +1,14 @@
+package com.graduation.carrentalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalWebApplication.class, args);
+	}
+
+}
+
